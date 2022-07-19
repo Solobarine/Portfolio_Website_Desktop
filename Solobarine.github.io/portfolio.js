14f86b3f44e50ae0d5c1f0c63b484077154cc8cf
Solly <solobarine@gmail.com>
2022-07-19 08:03:32 +0100

    Add collapsible menu and styling to webpage

---
+++ Solobarine.github.io/portfolio.js
@@ -0,0 +1,48 @@
+let open = document.querySelector("#bars");
+let imgButton = document.querySelector(".img-button");
+let page = document.querySelector(".pop-up");
+let portfolio = document.querySelector(".the-nav1");
+let about = document.querySelector(".the-nav2");
+let contact = document.querySelector(".the-nav3");
+
+
+open.addEventListener('click', function(){
+  if(page.className === 'pop'){
+    page.className = '';
+  }else{
+    page.className = 'pop';
+  }
+});
+
+imgButton.addEventListener('click', function(){
+    if(page.className === ''){
+        page.className = 'pop';
+    }else{
+        page.className = '';
+    }
+    });
+    
+portfolio.addEventListener('click', function(){
+    if(page.className === ''){
+        page.className = 'pop';
+    }else{
+        page.className = '';
+    }
+    });
+    
+about.addEventListener('click', function(){
+    if(page.className === ''){
+        page.className = 'pop';
+    }else{
+        page.className = '';
+    }
+    });
+    
+contact.addEventListener('click', function(){
+    if(page.className === ''){
+        page.className = 'pop';
+    }else{
+        page.className = '';
+    }
+    });
+    
