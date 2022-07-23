2fd26e81a818da47d0427e1305a1afe7f719fa11
Solly <solobarine@gmail.com>
2022-07-23 07:58:04 +0100

    Fix linter errors

--- Solobarine.github.io/portfolio.js
+++ Solobarine.github.io/portfolio.js
@@ -1,55 +1,53 @@
 // Menu Section
-let open = document.querySelector('#bars');
-let imgButton = document.querySelector(".img-button");
-let page = document.querySelector(".pop-up");
-let portfolio = document.querySelector(".the-nav1");
-let about = document.querySelector(".the-nav2");
-let contact = document.querySelector(".the-nav3");
-let header = document.querySelector('.header')
+const open = document.querySelector('#bars');
+const imgButton = document.querySelector('.img-button');
+const page = document.querySelector('.pop-up');
+const portfolio = document.querySelector('.the-nav1');
+const about = document.querySelector('.the-nav2');
+const contact = document.querySelector('.the-nav3');
+const header = document.querySelector('.header');
 
-
-open.addEventListener('click', function(){
-  if(page.className === 'pop'){
+open.addEventListener('click', () => {
+  if (page.className === 'pop'){
     page.className = '';
     header.style.display = 'none';
-  }else{
+  } else {
     page.className = 'pop';
   }
 });
 
-imgButton.addEventListener('click', function(){
-    if(page.className === ''){
-        page.className = 'pop';
-        header.style.display = 'block';
-    }else{
-        page.className = '';
-    }
-    });
-    
-portfolio.addEventListener('click', function(){
-    if(page.className === ''){
-        page.className = 'pop';
-        header.style.display = 'block';
-    }else{
-        page.className = '';
-    }
-    });
-    
-about.addEventListener('click', function(){
-    if(page.className === ''){
-        page.className = 'pop';
-        header.style.display = 'block';
-    }else{
-        page.className = '';
-    }
-    });
-    
-contact.addEventListener('click', function(){
-    if(page.className === ''){
-        page.className = 'pop';
-        header.style.display = 'block';
-    }else{
-        page.className = '';
-    }
-    });
-    
+imgButton.addEventListener('click', () => {
+  if(page.className === ''){
+    page.className = 'pop';
+    header.style.display = 'block';
+  } else {
+    page.className = '';
+  }
+});
+
+portfolio.addEventListener('click', () => {
+  if(page.className === ''){
+    page.className = 'pop';
+    header.style.display = 'block';
+  } else {
+    page.className = '';
+  }
+});
+
+about.addEventListener('click', () => {
+  if(page.className === ''){
+    page.className = 'pop';
+    header.style.display = 'block';
+  } else {
+    page.className = '';
+  }
+});
+
+contact.addEventListener('click', () => {
+  if (page.className === ''){
+     page.className = 'pop';
+    header.style.display = 'block';
+  } else {
+    page.className = '';
+  }
+});
